0a78d06a2bfd67f0297588cf95e528a029be0798
Rafael Palomar <rafaelpalomaravalos@gmail.com>
2014-04-06 09:52:02 +0200

    ENH: Added new example (source + widget)

---
+++ Example_Source_Widget.cxx
@@ -0,0 +1,56 @@
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkPolyData.h>
+#include <vtkSphereSource.h>
+#include <vtkSphereWidget.h>
+#include <vtkSphereRepresentation.h>
+#include <vtkCommand.h>
+#include <vtkPlaneSource.h>
+
+
+#include "vtkBezierSurfaceWidget.h"
+#include "vtkBezierSurfaceSource.h"
+
+int main(int argc, char *argv[])
+{
+  //Create a renderer, render window, and interactor
+  vtkSmartPointer<vtkRenderer> renderer = 
+      vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkRenderWindow> renderWindow = 
+      vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->AddRenderer(renderer);
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = 
+      vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+
+  vtkSmartPointer<vtkBezierSurfaceSource> bezierSource =
+    vtkSmartPointer<vtkBezierSurfaceSource>::New();
+  bezierSource->Update();
+
+  vtkSmartPointer<vtkBezierSurfaceWidget> widget = 
+    vtkSmartPointer<vtkBezierSurfaceWidget>::New();
+  widget->SetInteractor(renderWindowInteractor);
+  widget->SetSource(bezierSource);
+  widget->On();
+
+  vtkSmartPointer<vtkPolyDataMapper> mapper = 
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  mapper->SetInputConnection(bezierSource->GetOutputPort());
+  
+  vtkSmartPointer<vtkActor> actor = 
+    vtkSmartPointer<vtkActor>::New();
+  actor->SetMapper(mapper);
+
+  renderer->AddActor(actor);
+  renderer->SetBackground(1,1,1); // Background color white
+  
+  //Render and interact
+  renderWindow->Render();
+  renderWindowInteractor->Start();
+  
+  return EXIT_SUCCESS;
+}
