3543e50f7669b945a7a03c8fefe7f7d1b94b056e
Rafael Palomar <rafael.palomar@rr-research.no>
2014-04-07 18:14:52 +0200

    ENH: Extent of Bezier surface added to example

--- Example_Source_Widget.cxx
+++ Example_Source_Widget.cxx
@@ -11,6 +11,7 @@
 #include <vtkCommand.h>
 #include <vtkPlaneSource.h>
 
+#include <iostream>
 
 #include "vtkBezierSurfaceWidget.h"
 #include "vtkBezierSurfaceSource.h"
@@ -18,39 +19,42 @@
 int main(int argc, char *argv[])
 {
   //Create a renderer, render window, and interactor
-  vtkSmartPointer<vtkRenderer> renderer = 
+  vtkSmartPointer<vtkRenderer> renderer =
     vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renderWindow = 
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
     vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
-  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = 
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
     vtkSmartPointer<vtkRenderWindowInteractor>::New();
   renderWindowInteractor->SetRenderWindow(renderWindow);
 
   vtkSmartPointer<vtkBezierSurfaceSource> bezierSource =
     vtkSmartPointer<vtkBezierSurfaceSource>::New();
+  bezierSource->SetBounds(-1,1,-1,4);
   bezierSource->Update();
+  std::cout << *bezierSource << std::endl;
 
-  vtkSmartPointer<vtkBezierSurfaceWidget> widget = 
+  vtkSmartPointer<vtkBezierSurfaceWidget> widget =
     vtkSmartPointer<vtkBezierSurfaceWidget>::New();
   widget->SetInteractor(renderWindowInteractor);
   widget->SetSource(bezierSource);
+  widget->SetHandleSize(100);
   widget->On();
 
-  vtkSmartPointer<vtkPolyDataMapper> mapper = 
+  vtkSmartPointer<vtkPolyDataMapper> mapper =
     vtkSmartPointer<vtkPolyDataMapper>::New();
   mapper->SetInputConnection(bezierSource->GetOutputPort());
-  
-  vtkSmartPointer<vtkActor> actor = 
+
+  vtkSmartPointer<vtkActor> actor =
     vtkSmartPointer<vtkActor>::New();
   actor->SetMapper(mapper);
 
   renderer->AddActor(actor);
   renderer->SetBackground(1,1,1); // Background color white
-  
+
   //Render and interact
   renderWindow->Render();
   renderWindowInteractor->Start();
-  
+
   return EXIT_SUCCESS;
 }
